6b5eb4df6f6f171d61556be58c1f726c14c67b30
Sue Zadeh <suezadeh.a@gmail.com>
2024-07-22 11:12:14 +1200

    login page

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = 'ExampleSecretKey'
 
 # Default configurations
 app.config['UPLOAD_FOLDER'] = 'app/static/uploads'
-app.secret_key = 'ExampleSecretKey'
 
 def register_routes():
-    from app import views
-    # from app import admin_views
-    # from app import staff_views
-    # from app import user
-    # from app import admin
-    # from app import staff
-    # from app import connect
+    from app import views  # Import all routes from views.py
 
 register_routes()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- app/views.py
+++ app/views.py
@@ -1,62 +1,57 @@
-from flask import Flask, redirect, url_for, render_template,request, session
+from flask import render_template, redirect, url_for, request, session
 from app import app
-
-app = Flask(__name__)
-app.secret_key = 'Example Secret Key (i should take it from a config file)'
 
 @app.route('/')
 def home():
-  return render_template('index.html')
- 
-@app.route("/login", methods = ['GET', 'POST'])
+    return render_template('index.html')
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-  if request.method == 'POST':
-    user = request.form['nm']
-    userRole = request.form['rl']
-    session['user'] = user   #Set the session's user
-    session['role'] = userRole #Set the session's role
-    return redirect(url_for('user')) #Redirect to the user page
-  else:
-    if "user" in  session:
-      return redirect(url_for('user'))
-    return render_template('login.html')
- #user   
+    if request.method == 'POST':
+        user = request.form['nm']
+        userRole = request.form['rl']
+        session['user'] = user  # Set the session's user
+        session['role'] = userRole  # Set the session's role
+        return redirect(url_for('user'))  # Redirect to the user page
+    else:
+        if "user" in session:
+            return redirect(url_for('user'))
+        return render_template('login.html')
+
 @app.route("/user")
 def user():
-  if "user" in session:
-    user = session["user"]
-    userRole = session["role"]
-    return f"<h1>{user}</h1>{user} {userRole}</h1>"
-  else:
-    return redirect(url_for('login'))
-  #staff   
+    if "user" in session:
+        user = session["user"]
+        userRole = session["role"]
+        return f"<h1>{user}</h1>{user} {userRole}</h1>"
+    else:
+        return redirect(url_for('login'))
+
 @app.route("/staff")
 def staff():
-  if "user" in session:
-    if session["role"] == "staff":   # check the role of the user
-      return render_template('staff.html')
+    if "user" in session:
+        if session["role"] == "staff":  # Check the role of the user
+            return render_template('staff.html')
+        else:
+            return "Illegal Access"  # Anyone else other than staff
     else:
-      return "Illegal Access"      # anyone else other than staff
-  else:
-    return redirect(url_for('login'))
-  
-  #admin
+        return redirect(url_for('login'))
+
 @app.route("/admin")
 def admin():
     if "user" in session:
-      if session["role"] == "admin":   # check the role of the user
-        return render_template('admin.html')
-      else:
-        return "Illegal Access"      # anyone else other than staff
+        if session["role"] == "admin":  # Check the role of the user
+            return render_template('admin.html')
+        else:
+            return "Illegal Access"  # Anyone else other than admin
     else:
-      return redirect(url_for('login'))
-   #logout 
-@app.route("/logout") 
+        return redirect(url_for('login'))
+
+@app.route("/logout")
 def logout():
-    session.pop('user', None)  #session is cleared at logout
+    session.pop('user', None)  # Session is cleared at logout
     session.pop('role', None)
     return redirect(url_for('login'))
-    
-    
+
 if __name__ == '__main__':
-      app.run(debug = True)
+    app.run(debug=True, port=5001)
